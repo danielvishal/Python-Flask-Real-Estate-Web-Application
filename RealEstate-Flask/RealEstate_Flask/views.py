6f9fb52a2db582e1368e08c68eaa66ff48fd2642
vishdan <vd.daniel@outlook.com>
2019-05-02 09:34:45 +0530

    *

--- RealEstate-Flask/RealEstate_Flask/views.py
+++ RealEstate-Flask/RealEstate_Flask/views.py
@@ -5,6 +5,9 @@ Routes and views for the flask application.
 from datetime import datetime
 from flask import render_template, request, Flask
 from RealEstate_Flask import app
+import pandas as pd
+
+
 
 @app.route('/')
 @app.route('/home')
@@ -23,7 +26,22 @@ def contact():
         'contact.html',
         title='Contact',
         year=datetime.now().year,
-        message='Your contact page.'
+        message='Your contact page.',
+        i=[1,2,3,4]
+    )
+
+@app.route('/land')
+def land():
+    df=pd.read_csv("land.csv",index_col=0)
+    num=len(df.index)
+    loca=df.Location.unique().tolist()
+    """Renders the contact page."""
+    return render_template(
+        'land.html',
+        num=num,
+        df=df,
+        loca=loca,
+        num_loca=len(loca)
     )
 
 @app.route('/search')
@@ -31,9 +49,7 @@ def search():
     """Renders the contact page."""
     return render_template(
         'search.html',
-        title='Contact',
-        year=datetime.now().year,
-        message='Your contact page.'
+        
     )
 
 
@@ -65,8 +81,7 @@ def login_post():
             continue
         if(password==pass_txt.strip()):
             return render_template(
-             'index.html',
-             title='About'
+             'adminindex.html'
              ) 
         else:
             fhand.close()
@@ -80,4 +95,44 @@ def login_post():
              'page-login.html',
              title='About'
              ) 
+  
     
+#ADMIN PAGES
+
+@app.route('/addland')
+def addland():
+    """Renders the addland page."""
+    return render_template(
+        'addland.html'
+    )
+
+@app.route('/addland',methods=['POST'])
+def addland_post():
+ 
+    location = request.form['location']
+    price = int(request.form['price'])
+    size = int(request.form['size'])
+    cost=price*size
+
+    df=pd.read_csv("land.csv",index_col=0)
+    loca=location.strip().upper()
+    index=int("".join(str(ord(c)) for c in loca))
+    dic={'Location':location,'Price':price,'Size':size,'Cost':cost}
+    add=pd.DataFrame(dic,index=[index])
+    df=df.append(add)
+    df.to_csv("land.csv")
+
+    return render_template(
+        'index.html'
+    )
+
+@app.route('/displand')
+def displand():
+    df=pd.read_csv("land.csv")
+    num=len(df.index)
+    """Renders the addland page."""
+    return render_template(
+        'displand.html',
+        df=df,
+        num=num
+    )
